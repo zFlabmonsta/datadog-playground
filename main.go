e3deac6fb4337cfa7680927b55bbae6a33c9ed13
Peter Nguyen <peter.nguyen@zendesk.com>
2022-11-29 00:07:21 +1100

    chore: instrument datadog with tracer

--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func main() {
+	tracer.Start(
+		tracer.WithService("dd-toy"),
+		tracer.WithEnv("development"),
+	)
+	defer tracer.Stop()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(DataDogTracer())
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
 	http.ListenAndServe(":3000", r)
+}
+
+func DataDogTracer() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			span := tracer.StartSpan("web.request", tracer.ResourceName(r.RequestURI))
+			defer span.Finish()
+
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
 }
