43aa3d347e08e3d8dc384fa093d81f242b828125
Peter Nguyen <peter.nguyen@zendesk.com>
2022-12-04 00:06:06 +1100

    chore: enabling go profiling

--- web/main.go
+++ web/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
 )
 
 func main() {
-	tracer.Start(
-		tracer.WithService("dd-toy"),
-		tracer.WithEnv("development"),
-	)
+	tracer.Start()
 	defer tracer.Stop()
+
+	err := profiler.Start(
+		profiler.WithProfileTypes(
+			profiler.CPUProfile,
+			profiler.HeapProfile,
+		),
+	)
+	if err != nil {
+		log.Fatalf("error starting profiler: %v", err)
+	}
+	defer profiler.Stop()
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
